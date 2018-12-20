0802e914ef367ec419ab55906c3cf2497e319f36
Paul Ronga <paul.ronga@laposte.net>
2018-12-20 11:27:14 +0100

    prod route links

--- src/App.js
+++ src/App.js
@@ -42,18 +42,18 @@ import ShareButtons from './components/ShareButtons';
 // pour ça il faudrait utiliser un <HashRouter>
 const useRewriting = true;
 // const localDev = /localhost$/.test(host);
-/*
-prod
 const routePath = useRewriting ? '/guide-des-series/series/:uniquekey' : '/guide-des-series/#:uniquekey';
 const routeHomepage = '/guide-des-series/';
 const childRoute = useRewriting ? "/guide-des-series/series/" : '/guide-des-series/#';
 const landingOnDetailTest = useRewriting ? /series\/series/ : /series\/#/;
-*/
-/* dev */
+
+/*
+// dev
 const routePath = useRewriting ? '/series/:uniquekey' : '/guide-des-series/#:uniquekey';
 const routeHomepage = '/';
 const childRoute = useRewriting ? "/series/" : '/guide-des-series/#';
 const landingOnDetailTest = useRewriting ? /series/ : /series\/#/;
+*/
 
 require("typeface-montserrat");
 var sortJsonArray = require("sort-json-array");
